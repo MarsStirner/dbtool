e0a20e33acb758792a3f1b1c51fa364d43c2e6bd
swod <roman.s3000@gmail.com>
2012-10-23 13:56:00 +0400

    version 60

---
+++ updates/db060.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+- Коллекция необходимых изменений для ЗНР по ВМП
+'''
+
+
+def upgrade(conn):
+    c = conn.cursor()
+    
+    sql = u'''
+ALTER TABLE `rbTest`
+ADD COLUMN `deleted` TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'Отметка удаления записи' AFTER `name`;
+'''
+    c.execute(sql)
+        
+
+def downgrade(conn):
+    pass
