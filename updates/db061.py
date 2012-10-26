591a7e06687f2d89496fd29ca7f2e2c2bfa67d3e
Viruzzz-kun <viruzzz.soft@gmail.com>
2012-10-26 17:09:45 +0400

    В версию 61 добавлена таблица action_document для RichEditor'а

--- updates/db061.py
+++ updates/db061.py
@@ -89,6 +89,24 @@ END;
 ''' % (config['username'], config['host'])
 
     c.execute(sql)
+    
+    sql = u'''
+CREATE TABLE `action_document` (
+    `id` INT(11) NOT NULL AUTO_INCREMENT COMMENT 'Идентификатор документа',
+    `action_id` INT(11) NOT NULL COMMENT 'Идентификатор Действия {Action}',
+    `modify_date` DATETIME NOT NULL COMMENT 'Дата изменения документа',
+    `template_id` INT(11) NOT NULL COMMENT 'Шаблон, по которому составлен документ {rbPrintTemplates}',
+    `document` MEDIUMBLOB NOT NULL COMMENT 'Документ, сжатый gzip',
+    PRIMARY KEY (`id`),
+    INDEX `action_id` (`action_id`),
+    INDEX `FK_action_document_rbprinttemplate` (`template_id`),
+    CONSTRAINT `FK_action_document_action` FOREIGN KEY (`action_id`) REFERENCES `action` (`id`),
+    CONSTRAINT `FK_action_document_rbprinttemplate` FOREIGN KEY (`template_id`) REFERENCES `rbprinttemplate` (`id`)
+)
+COMMENT='Таблица отрендеренных документов от Action\'ов, версионная. Документы хранятся сжатыми алгоритмом gzip.'
+COLLATE='utf8_general_ci'
+ENGINE=InnoDB;
+'''
         
 
 def downgrade(conn):
