c7aaf2383fd9242da982d485e136ea6a83b38d10
swod <roman.s3000@gmail.com>
2012-11-05 21:51:00 +0400

    Version 62 update

--- updates/db062.py
+++ updates/db062.py
@@ -4,7 +4,8 @@
 from __future__ import unicode_literals, print_function
 
 __doc__ = '''\
-- Коллекция необходимых изменений для ЗНР по ВМП
+- Потерянные столбцы из функционала 6098
+- Таблица для настройки проверяемых действий при закрытии обращений
 '''
 
 
@@ -22,6 +23,23 @@ ALTER TABLE `Person` ADD `maxOverQueue` TINYINT DEFAULT 0 AFTER `typeTimeLinePer
 '''
     c.execute(sql)
     
+    sql = u'''
+CREATE TABLE `ActionType_EventType_check` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `actionType_id` int(11) NOT NULL,
+  `eventType_id` int(11) NOT NULL,
+  `related_actionType_id` int(11) DEFAULT NULL,
+  `relationType` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `fk_actionType_id` (`actionType_id`),
+  KEY `fk_eventType_id` (`eventType_id`),
+  KEY `fk_related_ActionType_id` (`related_actionType_id`),
+  CONSTRAINT `fk_actionType_id` FOREIGN KEY (`actionType_id`) REFERENCES `ActionType` (`id`),
+  CONSTRAINT `fk_eventType_id` FOREIGN KEY (`eventType_id`) REFERENCES `EventType` (`id`),
+  CONSTRAINT `fk_related_ActionType_id` FOREIGN KEY (`related_actionType_id`) REFERENCES `ActionType` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='Проверка на наличие типа действия для конкретных типов обращений'
+'''
+    c.execute(sql) 
 
 def downgrade(conn):
     pass
