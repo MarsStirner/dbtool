e1efe5bf5e916b012037dfc1b5c5337fe026df88
viruzzz-k <viruzzz.soft+korus@gmail.com>
2012-12-19 19:56:22 +0400

    Update 65 fix v3

--- updates/db065.py
+++ updates/db065.py
@@ -75,7 +75,7 @@ ALTER TABLE `Action` ADD COLUMN `parentAction_id` INT(11) NULL DEFAULT NULL COMM
 INSERT INTO `ActionType` (`class`, `code`, `name`, `title`, `flatCode`,
 `createDatetime`, `modifyDatetime`, `sex`, `age`, `office`, `showInForm`, `genTimetable`, `context`, `defaultPlannedEndDate`)
 VALUES (-1, 'PRPA_IN402006UV02', 'Отмена сообщения о госпитализации (поступлении)', 'Отмена сообщения о госпитализации', 'del_received', 
-CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, '0-969', '', 0, 0, '', '0000-00-00');
+CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, '0-969', '', 0, 0, '', '2);
 '''
     c.execute(sql)
     
@@ -83,7 +83,7 @@ CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, '0-969', '', 0, 0, '', '0000-00-00');
 INSERT INTO `ActionType` (`class`, `code`, `name`, `title`, `flatCode`,
 `createDatetime`, `modifyDatetime`, `sex`, `age`, `office`, `showInForm`, `genTimetable`, `context`, `defaultPlannedEndDate`)
 VALUES (-1, 'PRPA_IN402006UV02', 'Отмена сообщения о переводе между отделениями внутри стационара', 'Отмена сообщения переводе', 'del_moving',
-CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, '0-969', '', 0, 0, '', '2');
+CURRENT_TIMESTAMP, CURRENT_TIMESTAMP, 0, '0-969', '', 0, 0, '', 2);
 '''
     c.execute(sql)
     
