aa560696221bd70b5077e936e1cb35f8705966cc
A Gusenko <algusen@gmail.com>
2013-01-18 14:19:14 +0400

    Update 73
    Измения БД для WebMIS (1.0.47)

---
+++ updates/db073.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+- добавляем записи для ActionType = 118 (Выписка) в rbCoreActionProperty
+'''
+
+
+def upgrade(conn):
+    global config    
+    c = conn.cursor()
+    
+    sql = u'''
+INSERT INTO rbCoreActionProperty VALUES 
+(158, 118, 'Дата следующей госпитализации(в текущем году)', 20174), 
+(159, 118, 'Отделение госпитализации', 36081), 
+(160, 118, 'Источник финансирования следующей госпитализации', 36082);
+'''
+    c.execute(sql)
+    
+  
+
+
+    
+def downgrade(conn):
+    pass
