45b77cad72d41720fca48713d939256fd4bb9da9
viruzzz-kun <viruzzz.soft@gmail.com>
2013-03-27 13:17:16 +0400

    Update 94: Справочник УФМС

---
+++ updates/db094.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''
+Справочник УФМС
+'''
+def upgrade(conn):
+    global config
+    c = conn.cursor()
+    sql = u'''
+CREATE TABLE `rbUFMS` (
+    `id` INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+    `code` VARCHAR(50) NOT NULL,
+    `name` VARCHAR(256) NOT NULL,
+    PRIMARY KEY (`id`)
+)
+COMMENT='Справочник УФМС'
+COLLATE='utf8_bin'
+ENGINE=InnoDB;
+'''   
+    c.execute(sql)    
+    c.close()
+    
+def downgrade(conn):
+    pass
+
