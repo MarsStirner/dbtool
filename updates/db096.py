70ab4939206f365e5ca224cfc184d5a07008148c
Viruzzz-kun <viruzzz.soft@gmail.com>
2013-03-28 23:48:39 +0400

    Update96: Update for MIS-316 & MIS-318

--- updates/db096.py
+++ updates/db096.py
@@ -8,6 +8,49 @@ __doc__ = '''\
 Добавление полей "Обязательное" и "Только для чтения" к типам свойств действия
 '''
 
+__author__ = 'viruzzz-kun'
+
+query_template_1 = u'''
+UPDATE `ActionPropertyType`
+SET
+    mandatory = '%s',
+    readOnly = '%s'
+WHERE 
+    `code` LIKE '%s' AND
+    `actionType_id` IN (
+        SELECT `id`
+        FROM `ActionType`
+        WHERE `flatCode` LIKE '%s'
+    )
+'''
+replacements_1 = (
+    (1, 0, u'orgStructDirection', u'received'),
+    (1, 0, u'hospOrgStruct',      u'moving'),
+    (1, 0, u'hospOrgStruct',      u'leaved'),
+)
+
+
+query_template_2 = u'''
+UPDATE `ActionPropertyType`
+SET
+    code = '%s',
+    mandatory = '%s',
+    readOnly = '%s'
+WHERE 
+    `name` LIKE '%s' AND
+    `actionType_id` IN (
+        SELECT `id`
+        FROM `ActionType`
+        WHERE `flatCode` LIKE '%s'
+    )
+'''
+
+replacements_2 = (
+    (u'nomen',    1, 0, u'Наименование',  u'prescription'),
+    (u'dosage',   0, 1, u'Дозировка',     u'prescription'),
+    (u'units',    0, 1, u'Единицы',       u'prescription'),
+)
+
 
 def upgrade(conn):
     global config        
@@ -24,6 +67,23 @@ def upgrade(conn):
     except:
         print('''Column `ActionPropertyType`.`readOnly` already exists.''')    
         traceback.print_exc()
+
+    c.execute(u"SET SQL_SAFE_UPDATES=0;")
+
+    for rep in replacements_1:
+        query = query_template_1 % rep
+        c.execute(query)
+        if c.rowcount < 1:
+            print (u'Не удалось установить значения для свойства "%s" типа действия "%s". Требуется ручное вмешательство.' % (rep[2], rep[3]))
+
+    for rep in replacements_2:
+        query = query_template_2 % rep
+        c.execute(query)
+        if c.rowcount < 1:
+            print (u'Не удалось установить значения для свойства "%s" типа действия "%s". Требуется ручное вмешательство.' % (rep[3], rep[4]))
+
+
+    c.execute(u"SET SQL_SAFE_UPDATES=1;")
 
     c.close()
 
