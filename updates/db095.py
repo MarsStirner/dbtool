35cf8a6e977cdc30f117751195f412df25fba9e6
plakrisenko <PLakrisenko@korusconsulting.ru>
2013-03-28 16:15:15 +0400

    update 95

---
+++ updates/db095.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+Изменение необходимое для отображения кнопки дежурного врача;
+Изменение необходимое для работы справочника 'Орагнизации';
+'''
+
+
+def upgrade(conn):
+    global config        
+    c = conn.cursor()
+
+    try:
+        c.execute(u'''ALTER TABLE Organisation ADD COLUMN isOrganisation TINYINT(1) NOT NULL DEFAULT '0' COMMENT 'является Организацией'  AFTER `miacCode` ;''')
+    except:
+        print('''Column 'isOrganisation' already exists.''')    
+
+
+    sql = u'''
+UPDATE `ActionType`
+SET
+    flatCode = 'dutyDoctor'
+WHERE
+    `deleted`= 0 and `title` LIKE 'Осмотр дежурного врача' 
+'''
+
+    c.execute(sql)        
+    c.close()
+
+
+def downgrade(conn):
+    pass
