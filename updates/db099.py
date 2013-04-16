f3aeeb5437ecee9f3073064126aeca0eed307bed
viruzzz-kun <viruzzz.soft@gmail.com>
2013-04-16 17:26:53 +0400

    Добавил закрывание курсора. Думаю, из-за него какие-то проблемы есть.

--- updates/db099.py
+++ updates/db099.py
@@ -14,16 +14,16 @@ medDoc_class = 0
 def upgrade(conn):
     global config        
     c = conn.cursor()
-    
+
     c.execute(u'''SELECT id FROM ActionType where code="%s" and group_id IS NULL''' % direction_code)
-    
+
     result = c.fetchall()
     if result:
         direction_at_ids = [res[0] for res in result]
         ids_cond = ','.join([str(id_) for id_ in direction_at_ids])
         c.execute(u'''UPDATE ActionType SET class=%s where id IN (%s)''' % (medDoc_class, ids_cond))
         c.execute(u'''UPDATE ActionType SET class=%s where group_id IN (%s)''' % (medDoc_class, ids_cond))
-    
-        
+    c.close()
+
 def downgrade(conn):
     pass
