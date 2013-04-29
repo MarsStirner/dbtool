bc7bc3fbcf6f5316b6d7f27ef2c71bbea235ba64
viruzzz-kun <viruzzz.soft@gmail.com>
2013-04-29 19:39:43 +0400

    Update104Fix 2 separate sql statements

--- updates/db104.py
+++ updates/db104.py
@@ -9,35 +9,35 @@ __doc__ = '''\
 - Расширение кода у rbPolicyType
 '''
 
-
-def upgrade(conn):
-    global config
-    c = conn.cursor()
-
-    sql = u'''
+sqls = [
+ur'''
 ALTER TABLE `rbRequestType` ADD COLUMN `relevant` TINYINT(1) NOT NULL DEFAULT 1;
-'''
-    try:
-        c.execute(sql)
-    except OperationalError, e:
-        print(e)
-        pass
-
-    sql = u'''
+''',
+ur'''
 ALTER TABLE `rbPolicyType`
     ALTER `code` DROP DEFAULT,
     ALTER `name` DROP DEFAULT;
+''',
+ur'''
 ALTER TABLE `rbPolicyType`
     CHANGE COLUMN `code` `code` VARCHAR(50) NOT NULL COMMENT 'Код' AFTER `id`,
     CHANGE COLUMN `name` `name` VARCHAR(256) NOT NULL COMMENT 'Наименование' AFTER `code`,
     DROP INDEX `code`,
     ADD UNIQUE INDEX `code` (`code`);
 '''
-    try:
-        c.execute(sql)
-    except OperationalError, e:
-        print(e)
-        pass
+]
+
+
+def upgrade(conn):
+    global config
+    c = conn.cursor()
+
+    for sql in sqls:
+        try:
+            c.execute(sql)
+        except OperationalError, e:
+            print(e)
+
     c.close()
 
 def downgrade(conn):
