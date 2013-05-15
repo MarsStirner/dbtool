0480492ee1138f1f8d19a84a049a052f88e7b258
szagrebelny <szgrebelny@korusconsulting.ru>
2013-05-15 10:44:06 +0400

    Set 'readOnly' for some TRFU properties

--- updates/db108.py
+++ updates/db108.py
@@ -14,7 +14,15 @@ simple_queries = \
 (
 u'''UPDATE `ActionPropertyType` SET `valueDomain`='TRFU_OIR', typeName = "Table" WHERE `code`='trfuReqBloodCompPasport' ''',
 u'''UPDATE `rbAPTableField` SET `fieldName`='trfu_blood_comp' WHERE  `fieldName`='trfu_comp_id' ''',
-u'''UPDATE `rbAPTableField` SET `fieldName`='comp_type_id' WHERE  `fieldName`='comp_type' '''
+u'''UPDATE `rbAPTableField` SET `fieldName`='comp_type_id' WHERE  `fieldName`='comp_type' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 1, `mandatory`= 0  WHERE `code` like 'trfu%' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 1  WHERE `code` like 'trfuReqBloodCompDiagnosis' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 1  WHERE `code` like 'trfuReqBloodCompId' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 1  WHERE `code` like 'trfuReqBloodCompType' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 0  WHERE `code` like 'trfuReqBloodCompValue' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 0  WHERE `code` like 'trfuReqBloodCompDose' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 1  WHERE `code` like 'trfuReqBloodCompRootCause' ''',
+u'''UPDATE `ActionPropertyType` SET `readOnly`= 0, `mandatory`= 0  WHERE `code` like 'trfuProcedureDonor' '''
 )
 
 def upgrade(conn):
