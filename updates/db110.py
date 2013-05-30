049476eb4cc1542a149504b67b91552e30553b48
swod <roman.s3000@gmail.com>
2013-05-30 15:40:09 +0400

    Version 110 update

--- updates/db110.py
+++ updates/db110.py
@@ -13,10 +13,21 @@ def upgrade(conn):
     global config        
     c = conn.cursor()
     
-    c.execute('SELECT id FROM ActionType WHERE id IN (SELECT DISTINCT group_id FROM ActionType) AND deleted=1')
-    ids = [str(rec[0]) for rec in c.fetchall()]
-    c.execute('UPDATE ActionType SET deleted=1 WHERE group_id IN (%s)' % ','.join(ids))
-        
+    c.execute('SELECT id, group_id, deleted FROM ActionType') 
+    all_records = [(int(id_), gr_id, d) for id_, gr_id, d in c.fetchall()]
+    all_groups = [gr_id for id_, gr_id, d in all_records]
+    deleted_groups = set([id_ for id_, gr_id, d in all_records if id_ in all_groups and d == 1])
 
+    ids_to_delete = set()
+    while deleted_groups:
+        check_id = deleted_groups.pop()
+        for id_, gr_id, d in all_records:
+            if gr_id == check_id:
+                ids_to_delete.add(id_)
+                deleted_groups.add(id_)
+    ids_str = [str(id_) for id_ in list(ids_to_delete)]
+    c.execute('UPDATE ActionType SET deleted=1 WHERE id IN (%s)' % ','.join(ids_str))
+            
+    
 def downgrade(conn):
     pass
