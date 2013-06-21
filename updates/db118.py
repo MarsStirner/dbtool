0f73887071c7ae72cb55d784f0036a2bb4abc66f
szagrebelny <szgrebelny@korusconsulting.ru>
2013-06-21 13:51:05 +0400

    HS integration

--- updates/db118.py
+++ updates/db118.py
@@ -25,6 +25,44 @@ def upgrade(conn):
        ENGINE=InnoDB;
     '''
     c.execute(sql)
+
+    sql = u'''
+        CREATE TABLE IF NOT EXISTS `PatientsToHS` (
+            `client_id` INT(11) NOT NULL COMMENT 'Идентификатор события {Event.event_id}',
+            `sendTime` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Время следующей отсылки в HealthShare',
+            `errCount` INT(11) NOT NULL DEFAULT 0 COMMENT 'Количество неудачных попыток',
+            `info` VARCHAR(1024) NULL DEFAULT NULL COMMENT 'Сообщение об ошибке',
+	    PRIMARY KEY (`client_id`),
+	   CONSTRAINT `FK__Client` FOREIGN KEY (`client_id`) REFERENCES `Client` (`id`)
+        )
+        COMMENT='события для передачи в HealthShare'
+        COLLATE='utf8_general_ci'
+       ENGINE=InnoDB;
+    '''
+    c.execute(sql)
+
+    sql = u'''
+    	CREATE DEFINER=%s PROCEDURE `SendClientToHS`(IN `aClient` INT)
+    	BEGIN
+    	   INSERT IGNORE INTO PatientsToHS SET PatientsToHS.client_id = aClient;
+	END;''' %config['definer']
+    c.execute(sql)
+
+    triggerEvents = ["Insert", "Update"]
+    clientInfoTables = ["ClientAddress", "ClientContact", "ClientDocument", "ClientPolicy"] 
+    sqlTemplate = u''' 
+          CREATE DEFINER=%s TRIGGER `on%s%s` AFTER %s ON `%s` FOR EACH ROW BEGIN
+             CALL SendClientToHS(NEW.client_id);   
+          END'''
+    for triggerEvent in triggerEvents:
+        sql = u''' 
+          CREATE DEFINER=%s TRIGGER `on%s%s` AFTER %s ON `%s` FOR EACH ROW BEGIN
+             CALL SendClientToHS(NEW.id);   
+          END''' % (config['definer'], triggerEvent, "Client", triggerEvent, "Client")
+        c.execute(sql)
+        for clientInfoTable in clientInfoTables:
+            c.execute(sqlTemplate % (config['definer'], triggerEvent, clientInfoTable, triggerEvent, clientInfoTable))
+
            
 def downgrade(conn):
     pass
