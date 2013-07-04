d3fe21a381c6df7c0e842bfcd50793d43d9ab57b
A Gusenko <algusen@gmail.com>
2013-07-04 18:00:21 +0400

    fix eroor
    now correctly

--- updates/db113.py
+++ updates/db113.py
@@ -8,50 +8,39 @@ __doc__ = '''\
 '''
 
 replacements = (
-    (u'CONS', u'ОСМОТР КОНСУЛЬТАТИВНЫЙ'),
-    (u'DIAG', u'ИНСТРУМЕНТАЛЬНЫЕ ИССЛЕДОВАНИЯ'),
-)
-
-sql = u''' 
-    SELECT * FROM ActionType
-    WHERE class = 1 AND id IN(
-        SELECT data.id FROM (
-            SELECT id FROM ActionType WHERE 
-            name LIKE '%s'
-            )
-        as data); 
-    '''
+                (u'CONS', u'ОСМОТР КОНСУЛЬТАТИВНЫЙ'),
+                (u'DIAG', u'ИНСТРУМЕНТАЛЬНЫЕ ИССЛЕДОВАНИЯ'))
 
 def upgrade(conn):
     global config    
     c = conn.cursor()
     
-    c.execute(u"SET SQL_SAFE_UPDATES=0;") 
+   # c.execute(u"SET SQL_SAFE_UPDATES=0;") 
     
     for rep in replacements:
-        query = sql % rep[1]
+        query = u''' SELECT * FROM ActionType WHERE class = 1 AND id IN(SELECT data.id FROM (SELECT id FROM ActionType WHERE name LIKE '%s') as data);''' % rep[1]
         c.execute(query)
         rows = c.fetchall()
         if len(rows) > 0:
             for row in rows:
                 setMnem(row[0], rep[0], conn)
     
-    c.execute(u"SET SQL_SAFE_UPDATES=1;")     
+    #c.execute(u"SET SQL_SAFE_UPDATES=1;")     
     c.close()    
   
 def setMnem(recordId, mnem, conn):
     c = conn.cursor()
-    c.execute(u'''SELECT * FROM ActionType where group_id="%s" and deleted=0''', recordId)
+    c.execute(u'''SELECT * FROM ActionType where group_id=%s and deleted=0''', recordId)
     rows = c.fetchall()
     
     if len(rows) > 0:
         for row in rows:
             setMnem(row[0], mnem, conn)
-    else:
-        try:
-            c.execute(u'''UPDATE ActionType SET mnem='%s' WHERE id =%s and deleted = 0''' % (mnem, recordId))
-        except:
-            print("Cann't set mnem for record ID: %s" % (recordId))
+   
+    try:
+        c.execute(u'''UPDATE ActionType SET mnem='%s' WHERE id =%s''' % (mnem, recordId))
+    except:
+        print("Cann't set mnem for record ID: %s" % (recordId))
         
 def downgrade(conn):
     pass
