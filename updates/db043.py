fa7b4a9e5516c9d5e5b96f9a3857db289ef7374c
swod <roman.s3000@gmail.com>
2013-10-25 17:03:43 +0400

    Fix update 43

--- updates/db043.py
+++ updates/db043.py
@@ -13,34 +13,34 @@ __doc__ = '''\
 def upgrade(conn):
     sqlsWithCheck = []
     sqlsWithCheck.append((u"""select id from ActionPropertyType
-            where actionType_id=(select id from ActionType at where at.name=\"Назначения\") and
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\") and
             name = \"Дозировка\" and
             typeName = \"String\" and
             deleted = 0""",
             u"""insert into ActionPropertyType(actionType_id,idx,name,descr,typeName,valueDomain,defaultValue,norm,sex,age)
-            values((select id from ActionType at where at.name=\"Назначения\"),
+            values((select id from ActionType at where at.name=\"Назначения\" AND deleted=0 LIMIT 1),
             2,\"Дозировка\", \"Дозировка лекарственного препарата\", \"String\",\"\",\"\",\"\",0,\"\")""",
             u""
         ))
     sqlsWithCheck.append((u"""select id from ActionPropertyType
-            where actionType_id=(select id from ActionType at where at.name=\"Назначения\") and
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\") and
             name = \"Доза\" and
             typeName = \"String\" and
             deleted = 0""",
             u"",
             u"""update ActionPropertyType
             set deleted = 1
-            where actionType_id = (select id from ActionType at where at.name=\"Назначения\")
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\")
             and name = \"Доза\" and
             typeName = \"String\" """
         ))
     sqlsWithCheck.append((u"""select id from ActionPropertyType
-            where actionType_id=(select id from ActionType at where at.name=\"Назначения\") and
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\") and
             name = \"Доза\" and
             typeName = \"Double\" and
             deleted = 0""",
             u"""insert into ActionPropertyType(actionType_id,idx,name,descr,typeName,valueDomain,defaultValue,norm,sex,age)
-            values((select id from ActionType at where at.name=\"Назначения\"),
+            values((select id from ActionType at where at.name=\"Назначения\" AND deleted=0 LIMIT 1),
             2,\"Доза\", \"Количество лекарственного препарата в в единицах измерения\", \"Double\",\"\",\"\",\"\",0,\"\")""",
             u""
         ))
@@ -60,16 +60,16 @@ def downgrade(conn):
     sql = [
         # Удаляем свойства типа действия Доза и Дозировка
         u"""delete from ActionPropertyType
-            where actionType_id = (select id from ActionType at where at.name=\"Назначения\") and
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\") and
             name = \"Дозировка\" and
             typeName = \"String\" """,
         u"""delete from ActionPropertyType
-            where actionType_id = (select id from ActionType at where at.name=\"Назначения\") and
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\") and
             name = \"Доза\" and
             typeName = \"Double\" """,
         u"""update ActionPropertyType
             set deleted = 0
-            where actionType_id = (select id from ActionType at where at.name=\"Назначения\")
+            where actionType_id IN (select id from ActionType at where at.name=\"Назначения\")
             and name = \"Доза\" and
             typeName = \"String\" """,
     ]
