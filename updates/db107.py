cceb8ddaec8b73e373bc8a5a7239c382f1638855
swod <roman.s3000@gmail.com>
2013-11-07 17:16:05 +0400

    Update 145

--- updates/db107.py
+++ updates/db107.py
@@ -8,29 +8,31 @@ __doc__ = '''\
 '''
 
 def upgrade(conn):
-    global config        
-    c = conn.cursor()
-    
-    def checkRecordExists(c, table, cond):
-        c.execute(u'''SELECT id FROM %s where %s ''' % (table, cond))
-        result = c.fetchone()
-        if result:
-            id_ = int(result[0])
-        else:
-            id_ = None
-        return id_
-    
-    AT_id = checkRecordExists(c, u'ActionType', u"flatCode='received'")
-    
-    if AT_id:
-        APT_id = checkRecordExists(c, u'ActionPropertyType', u"code='hospOrgStruct' and actionType_id=%s"%AT_id)
-        if not APT_id:
-            sql = u'''
-            INSERT INTO `ActionPropertyType` (`deleted`, `actionType_id`, `idx`, `name`, `descr`, `typeName`, `valueDomain`, `defaultValue`, `isVector`, `norm`, `sex`, `age`, `age_bu`, `age_bc`, `age_eu`, `age_ec`, `penalty`, `visibleInJobTicket`, `isAssignable`, `defaultEvaluation`, `toEpicrisis`, `code`, `mandatory`, `readOnly`) 
-            VALUES (0, %s, 0, 'Отделение поступления', 'Отделение, откуда поступил пациент, либо Приёмное отделение', 'OrgStructure', '', '', 0, '', 0, '', 0, 000, 0, 000, 0, 0, 0, 0, 0, 'hospOrgStruct', 0, 1);
-        ''' % AT_id
-            c.execute(sql)
-    c.close()
+    # отключено, будет добавлено в 133
+    pass
+#     global config
+#     c = conn.cursor()
+#
+#     def checkRecordExists(c, table, cond):
+#         c.execute(u'''SELECT id FROM %s where %s ''' % (table, cond))
+#         result = c.fetchone()
+#         if result:
+#             id_ = int(result[0])
+#         else:
+#             id_ = None
+#         return id_
+#
+#     AT_id = checkRecordExists(c, u'ActionType', u"flatCode='received'")
+#
+#     if AT_id:
+#         APT_id = checkRecordExists(c, u'ActionPropertyType', u"code='hospOrgStruct' and actionType_id=%s"%AT_id)
+#         if not APT_id:
+#             sql = u'''
+#             INSERT INTO `ActionPropertyType` (`deleted`, `actionType_id`, `idx`, `name`, `descr`, `typeName`, `valueDomain`, `defaultValue`, `isVector`, `norm`, `sex`, `age`, `age_bu`, `age_bc`, `age_eu`, `age_ec`, `penalty`, `visibleInJobTicket`, `isAssignable`, `defaultEvaluation`, `toEpicrisis`, `code`, `mandatory`, `readOnly`) 
+#             VALUES (0, %s, 0, 'Отделение поступления', 'Отделение, откуда поступил пациент, либо Приёмное отделение', 'OrgStructure', '', '', 0, '', 0, '', 0, 000, 0, 000, 0, 0, 0, 0, 0, 'hospOrgStruct', 0, 1);
+#         ''' % AT_id
+#             c.execute(sql)
+#     c.close()
 
 def downgrade(conn):
     pass
---
+++ updates/db145.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+
+__doc__ = '''\
+- Исправление даты создания APT "Отделение поступления" в AT "Поступление". Это
+влияет на правильную загрузку действий Поступлений в старых обращениях.
+- Исправление факапа 107-133 обновлений
+'''
+
+
+def upgrade(conn):
+    global tools
+    c = conn.cursor()
+
+    sql = u'''
+SELECT apt.id, at.createDatetime
+FROM ActionType at JOIN ActionPropertyType apt ON at.id = apt.actionType_id
+WHERE apt.code = '%s' AND at.flatCode = '%s'
+''' % ('orgStructStay', 'received')
+    num_rows = c.execute(sql)
+    if num_rows > 1:
+        print('Warning! Not unique APT')
+    apt_id, at_datetime = c.fetchone()
+    sql = u'''UPDATE ActionPropertyType SET createDatetime = '%s' WHERE id = %d''' % (at_datetime, apt_id)
+    c.execute(sql)
+
+    sql = u'''UPDATE ActionPropertyType SET deleted = 1 WHERE code = '%s'
+AND actionType_id = (SELECT id FROM ActionType WHERE flatCode = '%s' AND deleted = 0) ''' % ('hospOrgStruct', 'received')
+    c.execute(sql)
+
+    c.close()
+
+
+def downgrade(conn):
+    pass
