9969bbe360a543aac065ae3673e817961d33e26f
viruzzz-kun <viruzzz.soft@gmail.com>
2013-11-15 12:22:06 +0400

    Update-152-Fix

--- updates/db152.py
+++ updates/db152.py
@@ -10,17 +10,16 @@ def upgrade(conn):
     global config
     c = conn.cursor()
 
-    sql = u'''DROP TRIGGER `onUpdateDrugChart`'''
+    c.execute(u'''DROP TRIGGER IF EXISTS `onUpdateDrugChart`''')
 
-    c.execute(sql)
-
-    sql = u''' 
-          CREATE DEFINER=%s TRIGGER `onUpdateDrugChart` AFTER Update ON `DrugChart` FOR EACH ROW BEGIN
-             IF OLD.status != NEW.status THEN
-                CALL SendPrescriptionTo1C(NEW.id, IF(NEW.master_id IS NULL, 1, 0), OLD.status, NEW.status);   
-             END IF;
-          END'''%config['definer']
-    c.execute(sql)
+    c.execute(u'''CREATE DEFINER=%s
+TRIGGER `onUpdateDrugChart`
+AFTER Update ON `DrugChart` FOR EACH ROW
+BEGIN
+   IF OLD.status != NEW.status THEN
+      CALL SendPrescriptionTo1C(NEW.id, IF(NEW.master_id IS NULL, 1, 0), OLD.status, NEW.status);
+   END IF;
+END''' % config['definer'])
 
     c.close()
 
