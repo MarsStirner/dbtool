401337aa9f582c99738fcf16b088270fc650d507
swod <roman.s3000@gmail.com>
2013-12-02 14:26:51 +0300

    Обновление 151 нужно

--- updates/db151.py
+++ updates/db151.py
@@ -6,21 +6,21 @@ __doc__ = '''\
 Заполнение таблицы rbCoreActionProperty новыми свойствами поступления.
 '''
 
-# Выпилено
+
 def upgrade(conn):
     global config
     c = conn.cursor()
 
-#     c.execute(u'''INSERT INTO `rbCoreActionProperty` (`actionType_id`, `name`, `actionPropertyType_id`) 
-# 		SELECT `actionType_id`, `name`, `id` 
-# 		FROM `ActionPropertyType`
-# 		WHERE `actionType_id` IN (SELECT `id` FROM `ActionType` WHERE `flatCode` = 'received')
-# 			AND `deleted` = 0
-# 			AND `id` NOT IN (
-# 				SELECT `actionPropertyType_id`
-# 				FROM `rbCoreActionProperty`
-# 				WHERE `actionType_id` IN (SELECT `id` FROM `ActionType` WHERE `flatCode` = 'received'));''')
-#     c.close()
+    c.execute(u'''INSERT INTO `rbCoreActionProperty` (`actionType_id`, `name`, `actionPropertyType_id`) 
+SELECT `actionType_id`, `name`, `id` 
+FROM `ActionPropertyType`
+WHERE `actionType_id` IN (SELECT `id` FROM `ActionType` WHERE `flatCode` = 'received')
+AND `deleted` = 0
+AND `id` NOT IN (
+SELECT `actionPropertyType_id`
+FROM `rbCoreActionProperty`
+WHERE `actionType_id` IN (SELECT `id` FROM `ActionType` WHERE `flatCode` = 'received'));''')
+    c.close()
 
 
 def downgrade(conn):
