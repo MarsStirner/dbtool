44bf0293939c5ea2aea0336f982339272c32b10d
viruzzz-kun <viruzzz.soft@gmail.com>
2013-12-27 17:34:19 +0400

    Update 161-FIX: Более верная версионность справочника rbService

--- updates/db161.py
+++ updates/db161.py
@@ -11,9 +11,17 @@ def upgrade(conn):
     c = conn.cursor()
 
     c.execute(u"""ALTER TABLE `rbService`
-    ADD COLUMN `child_id` INT(11) NULL COMMENT 'Следующая версия записи справочника' AFTER `departCode`,
+    ADD COLUMN `group_id` INT(11) NULL COMMENT 'Первая версия записи справочника' AFTER `departCode`,
+    ADD INDEX `group_id` (`group_id`),
+    ADD CONSTRAINT `FK_rbService_rbService_2` FOREIGN KEY (`group_id`) REFERENCES `rbService` (`id`),
+
+    ADD COLUMN `child_id` INT(11) NULL COMMENT 'Следующая версия записи справочника' AFTER `group_id`,
     ADD INDEX `child_id` (`child_id`),
-    ADD CONSTRAINT `FK_rbService_rbService` FOREIGN KEY (`child_id`) REFERENCES `rbService` (`id`);""")
+    ADD CONSTRAINT `FK_rbService_rbService_1` FOREIGN KEY (`child_id`) REFERENCES `rbService` (`id`);""")
+
+    # Предполагается, что версионности никакой нет
+    c.execute(u"""UPDATE rbService SET group_id = id;""")
+
     c.close()
 
 def downgrade(conn):
