21920bbef2ef6d7c8714d51c04917d5ea7877dfc
Viruzzz-kun <viruzzz.soft@gmail.com>
2013-12-29 12:04:40 +0400

    Update 161-FIX: Проверка на невозможность выполнить апдейт. Необходима для совместимости с волшебным миром ФНКЦ

--- updates/db161.py
+++ updates/db161.py
@@ -10,15 +10,18 @@ def upgrade(conn):
     global config
     c = conn.cursor()
 
-    c.execute(u"""ALTER TABLE `rbService`
-    ADD COLUMN `group_id` INT(11) NULL COMMENT 'Первая версия записи справочника' AFTER `departCode`,
-    ADD INDEX `group_id_idx` (`group_id`, `idx`),
-    ADD CONSTRAINT `FK_rbService_rbService` FOREIGN KEY (`group_id`) REFERENCES `rbService` (`id`),
+    try:
+        c.execute(u"""ALTER TABLE `rbService`
+        ADD COLUMN `group_id` INT(11) NULL COMMENT 'Первая версия записи справочника' AFTER `departCode`,
+        ADD INDEX `group_id_idx` (`group_id`, `idx`),
+        ADD CONSTRAINT `FK_rbService_rbService` FOREIGN KEY (`group_id`) REFERENCES `rbService` (`id`),
 
-    ADD COLUMN `idx` INT(11) NOT NULL DEFAULT '0' COMMENT 'Номер версии' AFTER `group_id`;""")
-
-    # Предполагается, что версионности никакой нет
-    c.execute(u"""UPDATE rbService SET group_id = id;""")
+        ADD COLUMN `idx` INT(11) NOT NULL DEFAULT '0' COMMENT 'Номер версии' AFTER `group_id`;""")
+    except:
+        print(u'Похоже, что скрипт уже выполнен или произошла ошибка...')
+    else:
+        # Предполагается, что версионности никакой нет
+        c.execute(u"""UPDATE rbService SET group_id = id WHERE group_id IS NULL;""")
 
     c.close()
 
