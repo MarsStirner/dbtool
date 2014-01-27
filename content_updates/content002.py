ec167bff978423b6be145dbfcc4b34871eb1128d
MarmaladeSky <alexey.kislin@gmail.com>
2014-01-27 20:16:23 +0400

    update bak_lab mnemonic insert

--- content_updates/content002.py
+++ content_updates/content002.py
@@ -2,11 +2,34 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-
 __doc__ = '''\
--
+Введение дополнительной мнемоники BAK_LAB для исследований БАК-лаборатории
 '''
 
+import traceback
 
 def upgrade(conn):
-    return
+    global config
+    c = conn.cursor()
+
+    try:
+        c.execute(u"""UPDATE ActionType SET mnem = 'BAK_LAB' WHERE group_id=659 AND mnem = 'LAB';
+						 """)
+
+    except:
+         traceback.print_exc()
+
+    c.close()
+
+def downgrade(conn):
+    global config
+    c = conn.cursor()
+
+    try:
+        c.execute(u"""UPDATE ActionType SET mnem = 'LAB' WHERE group_id=659 AND mnem = 'BAK_LAB';
+						 """)
+
+    except:
+         traceback.print_exc()
+
+    c.close()
