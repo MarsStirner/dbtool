e59951b6084ea266c3dee151c461bcf7325a8b49
swod <roman.s3000@gmail.com>
2014-02-06 12:57:57 +0400

    Fix versions

--- updates/db165.py
+++ updates/db165.py
@@ -1,26 +1,20 @@
+#!/usr/bin/env python
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals, print_function
 
-import traceback
-
-__doc__ = '''\
-Новые параметры для справочника типов полиса
-логический признак, отвечающий за необходимость ввода серии
-логический признак, отвечающий за необходимость проверки длины номера полиса
-числовое поле, в котором будет указываться требуемая длина номера полиса
+__doc__ = '''
+Добавление OID организации из справочника 1.2.643.5.1.13.2.1.1.178 (MDR308)
 '''
 
 def upgrade(conn):
     global config
     c = conn.cursor()
 
-    try:
-        c.execute(u"""ALTER TABLE `rbPolicyType`
-                    ADD COLUMN `isPolicySerialRequired`  tinyint(1) NULL DEFAULT 0 AFTER `TFOMSCode`,
-                    ADD COLUMN `isPolicyNumberCheckRequired`  tinyint(1) NULL DEFAULT 0 AFTER `isPolicySerialRequired`,
-                    ADD COLUMN `policyNumberLength`  int(16) NULL DEFAULT 16 AFTER `isPolicyNumberCheckRequired`;""")
-    except:
-         traceback.print_exc()
+    sql = '''
+ALTER TABLE `Organisation`
+	ADD COLUMN `OID` VARCHAR(127) NULL COMMENT 'Код ЛПУ из справочника 1.2.643.5.1.13.2.1.1.178 (MDR308)' AFTER `uuid_id`;
+'''
+    c.execute(sql)
 
     c.close()
 
