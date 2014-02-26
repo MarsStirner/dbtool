8e797efeb58177ca9560d031d0c272b04aae91b8
MarmaladeSky <alexey.kislin@gmail.com>
2014-02-26 19:22:00 +0400

    fix of ClientDocument upgrading

--- content_updates/content005fnkc.py
+++ content_updates/content005fnkc.py
@@ -3,15 +3,13 @@
 from __future__ import unicode_literals, print_function
 
 __doc__ = '''\
-Удаление дубликатов документов, удостоверяющих личность и внесение foreign key
+Удаление дубликатов документов, удостоверяющих личность
 '''
 
 
 def upgrade(conn):
     global config
     c = conn.cursor()
-    c.execute(u"""DELETE FROM ClientDocument WHERE client_id NOT IN (SELECT id FROM Client);""")
-    c.execute(u"""ALTER TABLE ClientDocument ADD CONSTRAINT FK__ClientDocument_Client FOREIGN KEY (client_id) references Client(id) ON DELETE CASCADE ON UPDATE CASCADE;""")
     c.execute(u"""
     UPDATE ClientDocument SET deleted=1 WHERE id IN
         (SELECT id FROM (
