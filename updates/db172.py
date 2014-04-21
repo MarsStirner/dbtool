2fe1ff47b30da5a92ae9b5dc665e5bfb2205a5c6
MarmaladeSky <alexey.kislin@gmail.com>
2014-04-21 20:01:32 +0400

    add autosave data table

---
+++ updates/db172.py
@@ -0,0 +1,26 @@
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+Создание таблицы для хранения текста автосохранения полей ввода данных
+'''
+
+def upgrade(conn):
+    c = conn.cursor()
+    sql = '''
+    CREATE TABLE `AutoSaveStorage` (
+        id VARCHAR(60) NOT NULL,
+        user_id INT(11) NOT NULL,
+        modifyDatetime DATETIME NOT NULL,
+        text LONGTEXT NOT NULL,
+        PRIMARY KEY (id, user_id),
+        FOREIGN KEY `autoSaveStorage_user_id_to_Person` (user_id) REFERENCES Person (id) ON DELETE CASCADE ON UPDATE CASCADE
+    );
+    '''
+    c.execute(sql)
+
+def downgrade(conn):
+    c = conn.cursor()
+    sql = '''
+    DROP TABLE IF EXISTS `AutoSaveStorage`;
+    '''
+    c.execute(sql)
