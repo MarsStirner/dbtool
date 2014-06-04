0b27d260428e6bf4166de3af2d3ecfc81d6bb6ba
viruzzz-kun <viruzzz.soft@gmail.com>
2014-06-04 18:22:05 +0400

    Добавление столбцов для валидаторов в rbDocumentType и rbPolicyType

---
+++ updates/db182.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+- добавление столбцов для хранения форматов серий и номеров документов в виде регулярных выражений
+'''
+
+
+def upgrade(conn):
+    global config
+    c = conn.cursor()
+
+    c.execute('''
+ALTER TABLE `rbDocumentType`
+    ADD COLUMN `serial_regexp` VARCHAR(256) DEFAULT '.*' AFTER `TFOMSCode`,
+    ADD COLUMN `number_regexp` VARCHAR(256) DEFAULT '.*' AFTER `serial_regexp`;
+''')
+
+    c.execute('''
+ALTER TABLE `rbPolicyType`
+    ADD COLUMN `serial_regexp` VARCHAR(256) DEFAULT '.*' AFTER `TFOMSCode`,
+    ADD COLUMN `number_regexp` VARCHAR(256) DEFAULT '.*' AFTER `serial_regexp`;
+''')
+
+    c.execute(u'''
+UPDATE `rbDocumentType`
+    SET `serial_regexp` = '[IVXLCDM]+ [а-яА-Я]+'
+    WHERE `serial_format` = 1;
+''')
+
+    c.execute(u'''
+UPDATE `rbDocumentType`
+    SET `number_regexp` = '[IVXLCDM]+ [а-яА-Я]+'
+    WHERE `number_format` = 1;
+''')
+
+    c.execute(u'''
+UPDATE `rbDocumentType`
+    SET `serial_regexp` = '\d+'
+    WHERE `serial_format` = 2;
+''')
+
+    c.execute(u'''
+UPDATE `rbDocumentType`
+    SET `number_regexp` = '\d+'
+    WHERE `number_format` = 2;
+''')
+
+    c.close()
+
+
+def downgrade(conn):
+    pass
