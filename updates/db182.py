b8fb3cb2cea2f64ca96517c5633d00466e6db66d
SAntipov <santipov@korusconsulting.ru>
2014-06-30 19:01:15 +0400

    Сдвиг апдейтов, подготовка к мерджу из master

--- updates/db182.py
+++ updates/db182.py
@@ -1,1 +1,14 @@
-__author__ = 'santipov'
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+'''
+
+
+def upgrade(conn):
+    pass
+
+
+def downgrade(conn):
+    pass
