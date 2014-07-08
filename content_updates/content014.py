b3585ba493b10fda333093f7d7778d47c681f96f
vladislove <oleynick.v@gmail.com>
2014-07-08 21:09:18 +0400

    противоинфекционная терапия

---
+++ content_updates/content014.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+Данные для противоинфекционной терапии
+'''
+
+def upgrade(conn):
+    global config
+    c = conn.cursor()
+
+    sql = '''
+UPDATE ActionPropertyType SET valueDomain = ',Профилактика,Эмпирическая,Целенаправленная' WHERE code = 'infectTherapyType';
+'''
+    c.execute(sql)
+
+    sql = '''
+UPDATE ActionPropertyType SET valueDomain = ',Амбизом, Амикацин, Амоксиклав, Амоксициллина клавуланат, Амфолип, Аугментин, Ацикловир, Бисептол, Ванкомицин, Вифенд, Дифлюкан, Дориппрекс, Зивокс, Зиннат, Зовиракс, Изониазид, Кансидас, Кларитро/Азитромицин, Клиндамицин, Колистин, Максипим, Метроджил, Меронем, Метронидазол, Микамин, Микосист, Ноксафил, Панцеф, Роцефин, Сульперазон, Тазоцин, Тиенам, Флюконазол, Флагил, Фортум, Фторхинолоны, Цимевен, Эраксис, Эдицин' WHERE code = 'infectDrugName';
+'''
+    c.execute(sql)    
+
+    c.close()
