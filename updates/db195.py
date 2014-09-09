0e6ded896916cb8a46520ebde651dcb3f0b94011
swod <roman.s3000@gmail.com>
2014-09-09 18:22:37 +0400

    Fix 195

--- updates/db195.py
+++ updates/db195.py
@@ -15,7 +15,7 @@ def upgrade(conn):
         CREATE OR REPLACE DEFINER=%s VIEW `Proced_zapis_queue` AS
             select
                 `ScheduleClientTicket`.`id` AS `EventID`,
-                STR_TO_DATE(CONCAT(`Schedule`.`date`, ' ', `ScheduleTicket`.`begTime`), '%Y-%m-%d %H:%i:%s') AS `Datazapisi`,
+                STR_TO_DATE(CONCAT(`Schedule`.`date`, ' ', `ScheduleTicket`.`begTime`), '%Y-%m-%d %H:%i:%%s') AS `Datazapisi`,
                 `Office`.`code` AS `Kabinet`,
                 `ScheduleClientTicket`.`note` AS `Procedure`,
                 `Person`.`lastName` AS `Direction`,
