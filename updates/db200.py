af9de5c5aeaf3d85afc088f4759fa70ea3cfb2ef
plakrisenko <PLakrisenko@korusconsulting.ru>
2014-10-03 14:32:27 +0400

    сдвиг апдейтов

---
+++ updates/db200.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+'''
+
+
+def upgrade(conn):
+    pass
+
+
+def downgrade(conn):
+    pass
