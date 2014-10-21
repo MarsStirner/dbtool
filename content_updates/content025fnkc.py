d0947834eb2477b6be5bef957ce6a6e4e0ef8bc2
MarmaladeSky <alexey.kislin@gmail.com>
2014-10-21 14:47:18 +0400

    Добавления к новым свойствам ИФА значений параметров createPerson_id и modifyPerson_id

--- content_updates/content025fnkc.py
+++ content_updates/content025fnkc.py
@@ -10,8 +10,8 @@ def upgrade(conn):
     global tools
     c = conn.cursor()
     sql = '''INSERT INTO
-  ActionPropertyType (actionType_id, name, descr, typeName, valueDomain, defaultValue, code, norm, sex, age, createDatetime, modifyDatetime)
-  SELECT id, 'Комментарий', 'Комментарий к результатам лабораторного исследования', 'String', '','', 'comment', '', 0, '', NOW(), NOW() FROM ActionType at WHERE
+  ActionPropertyType (actionType_id, name, descr, typeName, valueDomain, defaultValue, code, norm, sex, age, createDatetime, modifyDatetime, createPerson_id, modifyPerson_id)
+  SELECT id, 'Комментарий', 'Комментарий к результатам лабораторного исследования', 'String', '','', 'comment', '', 0, '', NOW(), NOW(), 1, 1 FROM ActionType at WHERE
   ((at.group_id = 3900)
    OR EXISTS(SELECT * FROM ActionType WHERE id = at.group_id AND group_id = 3900))
 AND NOT EXISTS(SELECT * FROM ActionType WHERE group_id = at.id);'''
