4b104c80f3c2bd91a1992caa5884540e60e1dfa3
MarmaladeSky <alexey.kislin@gmail.com>
2014-10-24 17:21:09 +0400

    Добавление к каждому БАК исследованию свойства comment для хранения комментария

--- content_updates/content025fnkc.py
+++ content_updates/content025fnkc.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals, print_function
 
 __doc__ = '''\
-Добавление к каждому ИФА исследованию свойства comment для хранения комментария
+Добавление к каждому БАК исследованию свойства comment для хранения комментария
 '''
 
 def upgrade(conn):
@@ -14,6 +14,13 @@ def upgrade(conn):
   SELECT id, 'Комментарий', 'Комментарий к результатам лабораторного исследования', 'String', '','', 'comment', '', 0, '', NOW(), NOW(), 1, 1 FROM ActionType at WHERE
   ((at.group_id = 3900)
    OR EXISTS(SELECT * FROM ActionType WHERE id = at.group_id AND group_id = 3900))
+AND NOT EXISTS(SELECT * FROM ActionType WHERE group_id = at.id);'''
+    c.execute(sql)
+    sql = '''INSERT INTO
+  ActionPropertyType (actionType_id, name, descr, typeName, valueDomain, defaultValue, code, norm, sex, age, createDatetime, modifyDatetime, createPerson_id, modifyPerson_id)
+  SELECT id, 'Комментарий', 'Комментарий к результатам лабораторного исследования', 'String', '','', 'comment', '', 0, '', NOW(), NOW(), 1, 1 FROM ActionType at WHERE
+  ((at.group_id = 4027)
+   OR EXISTS(SELECT * FROM ActionType WHERE id = at.group_id AND group_id = 4027))
 AND NOT EXISTS(SELECT * FROM ActionType WHERE group_id = at.id);'''
     c.execute(sql)
     c.close()
