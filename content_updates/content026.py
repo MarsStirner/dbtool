0f86f4905f4426a2e8aec34331df63ef93c80680
Vladislav Oleynik <oleynick.v@gmail.com>
2014-12-15 18:31:04 +0300

    content026 fixes

--- content_updates/content026.py
+++ content_updates/content026.py
@@ -3,7 +3,7 @@
 from __future__ import unicode_literals, print_function
 
 __doc__ = '''\
-Данные об инфекционных осложнениях
+Изменения в разделе Противоинфекционная терапия
 '''
 
 def upgrade(conn):
