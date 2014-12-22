d700d8d09fbc5f1bf1bb5282408a15eb6e27095f
SergeyKorus <szgrebelny@korusconsulting.ru>
2014-12-22 11:55:16 +0400

    add update #201

---
+++ updates/db201.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+- Лист назначений:инфузионная терапия. Добавление более одного интервала назначения
+'''
+
+def upgrade(conn):
+    global config
+    c = conn.cursor()
+
+    sql = u'''CREATE TABLE `DrugIntervalCompParam` (
+	      `id` INT(11) NOT NULL AUTO_INCREMENT,
+              `drugChart_id` INT(11) NOT NULL,
+	      `drugComponent_id` INT(11) NOT NULL,
+	      `dose` FLOAT NULL DEFAULT NULL,
+	      `voa` FLOAT NULL DEFAULT NULL,
+	      PRIMARY KEY (`id`),
+	      CONSTRAINT `FK_DrugIntervalCompParam_DrugChart` FOREIGN KEY (`drugChart_id`) REFERENCES `DrugChart` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	      CONSTRAINT `FK_DrugIntervalCompParam_DrugComponent` FOREIGN KEY (`drugComponent_id`) REFERENCES `DrugComponent` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+        )
+        COLLATE='utf8_general_ci'
+        ENGINE=InnoDB'''
+
+    c.execute(sql)
+
+def downgrade(conn):
+    sql = u'''
+        DROP TABLE IF EXISTS `DrugIntervalCompParam`;
+    '''
+    c.execute(sql)
+
+    pass
