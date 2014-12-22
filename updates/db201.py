529954ae8a4b59646585e058e7798b4d602e3e83
SergeyKorus <szgrebelny@korusconsulting.ru>
2014-12-22 13:22:34 +0400

    update 201: add moa

--- updates/db201.py
+++ updates/db201.py
@@ -12,15 +12,20 @@ def upgrade(conn):
     c = conn.cursor()
 
     sql = u'''CREATE TABLE `DrugIntervalCompParam` (
-	      `id` INT(11) NOT NULL AUTO_INCREMENT,
-              `drugChart_id` INT(11) NOT NULL,
-	      `drugComponent_id` INT(11) NOT NULL,
-	      `dose` FLOAT NULL DEFAULT NULL,
-	      `voa` FLOAT NULL DEFAULT NULL,
-	      PRIMARY KEY (`id`),
-	      CONSTRAINT `FK_DrugIntervalCompParam_DrugChart` FOREIGN KEY (`drugChart_id`) REFERENCES `DrugChart` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
-	      CONSTRAINT `FK_DrugIntervalCompParam_DrugComponent` FOREIGN KEY (`drugComponent_id`) REFERENCES `DrugComponent` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
-        )
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`drugChart_id` INT(11) NOT NULL,
+	`drugComponent_id` INT(11) NOT NULL,
+	`dose` FLOAT NULL DEFAULT NULL,
+	`voa` FLOAT NULL DEFAULT NULL,
+	`moa_id` INT(11) NULL DEFAULT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `FK_DrugIntervalCompParam_DrugChart` (`drugChart_id`),
+	INDEX `FK_DrugIntervalCompParam_DrugComponent` (`drugComponent_id`),
+	INDEX `FK_DrugIntervalCompParam_rbMethodOfAdministration` (`moa_id`),
+	CONSTRAINT `FK_DrugIntervalCompParam_rbMethodOfAdministration` FOREIGN KEY (`moa_id`) REFERENCES `rbMethodOfAdministration` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `FK_DrugIntervalCompParam_DrugChart` FOREIGN KEY (`drugChart_id`) REFERENCES `DrugChart` (`id`) ON UPDATE CASCADE ON DELETE CASCADE,
+	CONSTRAINT `FK_DrugIntervalCompParam_DrugComponent` FOREIGN KEY (`drugComponent_id`) REFERENCES `DrugComponent` (`id`) ON UPDATE CASCADE ON DELETE CASCADE
+	)
         COLLATE='utf8_general_ci'
         ENGINE=InnoDB'''
 
