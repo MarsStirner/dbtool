8d5dc2c66581fd2433abe82d0b277810afc2898c
swod <roman.s3000@gmail.com>
2015-03-30 19:19:59 +0300

    Исправление сдвига после удаления дублирующегося обновления

--- content_updates/content034fnkc.py
+++ content_updates/content034fnkc.py
@@ -3,16 +3,25 @@
 from __future__ import unicode_literals, print_function
 
 __doc__ = '''\
-Заполнение createOnlyActionsWithinPriceList в EventType
+Миграция данных услуг типов действий в ActionType_Service
 '''
 
-MIN_SCHEMA_VERSION = 206
+MIN_SCHEMA_VERSION = 207
 
 
 def upgrade(conn):
     c = conn.cursor()
-    sql = '''UPDATE EventType
-SET createOnlyActionsWithinPriceList = 1 WHERE code IN ("02", "23")
-'''
+
+    c.execute('set sql_safe_updates = 0;')
+    sql = '''UPDATE ActionType_Service SET begDate = "1970-01-01";'''
     c.execute(sql)
+    sql = '''INSERT INTO ActionType_Service
+(`master_id`, `idx`, `service_id`, `begDate`, `endDate`) (
+    SELECT id, 0, service_id, "1970-01-01", NULL
+    FROM ActionType
+    WHERE deleted = 0 AND service_id IS NOT NULL
+)'''
+    c.execute(sql)
+    c.execute('set sql_safe_updates = 1;')
+
     c.close()
