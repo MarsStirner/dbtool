8e12cda17bbbd4117472571e15c2793254b7de0d
swod <roman.s3000@gmail.com>
2015-06-02 13:42:17 +0300

    - Обновления контента специфичные для фнкц (инфенционный контроль) вынесены в группу обновлений фнкц - исправлена ошибка создания расписания при миграции (content015)

--- content_updates/content023.py
+++ content_updates/content023.py
@@ -3,31 +3,9 @@
 from __future__ import unicode_literals, print_function
 
 __doc__ = '''\
-Данные для противоинфекционной терапии
+
 '''
+
 
 def upgrade(conn):
-    global config
-    c = conn.cursor()
-
-    c.execute('SET SQL_SAFE_UPDATES=0;')
-
-    sql = u'''
-SELECT id FROM LayoutAttribute WHERE code = 'NONTOGGLABLE';
-'''
-    c.execute(sql)
-    nontogglable = c.fetchone()
-
-    sql = u'''
-SELECT id FROM ActionPropertyType WHERE code = 'infectType';
-'''    
-    c.execute(sql)
-    infectType = c.fetchone()
-
-    c.execute('''
-        UPDATE LayoutAttributeValue SET value = 'false' WHERE layoutAttribute_id = {0} AND actionPropertyType_id = {1};
-        '''.format(nontogglable[0], infectType[0]))
-
-    c.execute('SET SQL_SAFE_UPDATES=1;')
-
-    c.close()
+    pass
