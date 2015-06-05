edf735bb1e363260cf1aceddcbab2d90b97415bb
swod <roman.s3000@gmail.com>
2015-06-05 19:56:48 +0300

    Удаление кривых дат в соцстатусах

--- updates/db214.py
+++ updates/db214.py
@@ -9,11 +9,17 @@ __doc__ = '''\
 
 def upgrade(conn):
     c = conn.cursor()
+    global tools
 
     sql = '''ALTER TABLE `ClientSocStatus`
 CHANGE COLUMN `begDate` `begDate` DATE NULL DEFAULT NULL COMMENT 'Дата начала действия записи' ;
 '''
     c.execute(sql)
+
+    sql = '''UPDATE `ClientSocStatus`
+SET `begDate` = NULL WHERE `begDate` = "0000-00-00";
+'''
+    tools.executeEx(c, sql, mode=['safe_updates_off'])
 
     c.close()
 
