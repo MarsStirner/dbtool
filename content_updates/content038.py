5977fc7c8f9e1ee66ee3615924b439b95215c7b2
swod <roman.s3000@gmail.com>
2015-08-25 14:59:59 +0300

    Исправления в обновлениях, связанных с квотами.

--- content_updates/content038.py
+++ content_updates/content038.py
@@ -2,38 +2,12 @@
 # -*- coding: utf-8 -*-
 
 __doc__ = '''\
-QuotaCatalog для ВМП за прошлый год
+
 '''
 
-MIN_SCHEMA_VERSION = 210
+MIN_SCHEMA_VERSION = 212
 
 
 def upgrade(conn):
     c = conn.cursor()
-
-    print(u'Добавляем QuotaCatalog за прошлый год')
-
-    sql = u'''
-INSERT INTO `QuotaCatalog` (`finance_id`, `createDatetime`, `modifyDatetime`, `begDate`, `endDate`)
-VALUES (7, NOW(), NOW(), '2014-01-01', '2014-12-31')
-'''
-    c.execute(sql)
-    last_id = c.lastrowid
-
-    print(u'Привязываем существующие строки QuotaType к QuotaCatalog за прошлый год')
-    sql = u'''UPDATE `QuotaType` SET `catalog_id`=%s''' % last_id
-    c.execute(sql)
-
-    sql = u'''
-ALTER TABLE `QuotaType` 
-DROP FOREIGN KEY `fk_catalog_id`;
-ALTER TABLE `QuotaType` 
-CHANGE COLUMN `catalog_id` `catalog_id` INT(11) NOT NULL COMMENT 'ссылка на справочник квот' ;
-ALTER TABLE `QuotaType` 
-ADD CONSTRAINT `fk_catalog_id`
-  FOREIGN KEY (`catalog_id`)
-  REFERENCES `QuotaCatalog` (`id`);
-'''
-    c.execute(sql)
-
     c.close()
