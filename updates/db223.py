04da4cdd11ba40a075d151cc65f5f4bc5b945077
SAntipov <santipov@korusconsulting.ru>
2015-09-29 13:11:26 +0300

    Добавление флага deleted в некоторые справочники

---
+++ updates/db223.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+Добавление флага deleted в перечисленные справочники
+'''
+
+rb_list = ['rbSpeciality', 'rbPost', 'rbFinance', 'rbResult', 'rbEventTypePurpose', 'rbRequestType', 'rbTreatmentType']
+
+
+def upgrade(conn):
+    with conn as c:
+        for rb_name in rb_list:
+            sql = '''ALTER TABLE `{table_name}`
+ADD COLUMN `deleted` TINYINT(1) NOT NULL DEFAULT '0';'''.format(table_name=rb_name)
+            c.execute(sql)
+
+
+def downgrade(conn):
+    with conn as c:
+        for rb_name in rb_list:
+            sql = '''ALTER TABLE `{table_name}` DROP COLUMN `deleted`;'''.format(table_name=rb_name)
+            c.execute(sql)
