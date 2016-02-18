4a898723a126bdd7224a5475b65143255259edee
plakrisenko <PLakrisenko@korusconsulting.ru>
2016-02-18 15:16:08 +0300

    Update db226.py, TMIS-668 связь соцстатуса пациента с записью в ClientWork

---
+++ updates/db226.py
@@ -0,0 +1,26 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+ Связь соцстатуса пациента с записью в ClientWork
+'''
+
+
+def upgrade(conn):
+    global config    
+    c = conn.cursor()
+
+    sql = '''ALTER TABLE `ClientWork`
+CHANGE COLUMN `rank_id` `rank_id` INT(10) UNSIGNED NULL COMMENT 'Звание военнослужащего. FlatDirectory№7' ,
+CHANGE COLUMN `arm_id` `arm_id` INT(10) UNSIGNED NULL COMMENT 'Род войск. FlatDirectory №6',
+ADD COLUMN `soc_status_id` INT(11) NOT NULL AFTER `arm_id`;
+'''
+    c.execute(sql)
+
+    c.close()
+
+
+def downgrade(conn):
+    pass
