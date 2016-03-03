223b23b8be681199afa1df32d08586b00227544e
plakrisenko <PLakrisenko@korusconsulting.ru>
2016-03-03 19:24:16 +0300

    update 227 настройка отображения раздела назначения лекарственных стредсв

---
+++ updates/db227.py
@@ -0,0 +1,24 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+Настройка отображения раздела назначения лекарственных стредств
+'''
+
+
+def upgrade(conn):
+    global config    
+    c = conn.cursor()
+
+    sql = '''ALTER TABLE `ActionType`
+ADD COLUMN `hasPrescriptions` TINYINT(1) NOT NULL DEFAULT '0' AFTER `layout`;
+'''
+    c.execute(sql)
+
+    c.close()
+
+
+def downgrade(conn):
+    pass
