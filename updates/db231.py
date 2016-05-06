348212135dc6838b2b19bd9a36e7f0026fbf8707
Михаил Мальков <m.malkov@bars-open.ru>
2016-05-06 11:41:13 +0300

    Update 231 For TMIS-1066

---
+++ updates/db231.py
@@ -0,0 +1,45 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals, print_function
+
+__doc__ = '''\
+- TMIS-1066 - Увеличение размеров полей для Аллергий и Мендикаментозных непереносимостей
+'''
+
+
+def upgrade(conn):
+    with conn as c:
+        c.execute('''
+ALTER TABLE `ClientAllergy`
+    ALTER `nameSubstance` DROP DEFAULT;
+ALTER TABLE `ClientAllergy`
+    CHANGE COLUMN `nameSubstance` `nameSubstance` TEXT NOT NULL COMMENT 'Наименование вещества' AFTER `client_id`,
+    CHANGE COLUMN `notes` `notes` TEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
+''')
+        c.execute('''
+ALTER TABLE `ClientIntoleranceMedicament`
+    ALTER `nameMedicament` DROP DEFAULT;
+ALTER TABLE `ClientIntoleranceMedicament`
+    CHANGE COLUMN `nameMedicament` `nameMedicament` TEXT NOT NULL COMMENT 'Название медикамента' AFTER `client_id`,
+    CHANGE COLUMN `notes` `notes` TEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
+''')
+
+
+def downgrade(conn):
+    with conn as c:
+        c.execute('''
+ALTER TABLE `ClientAllergy`
+    CHANGE COLUMN `nameSubstance` `nameSubstance` VARCHAR(128) NOT NULL COMMENT 'Наименование вещества' AFTER `client_id`,
+    CHANGE COLUMN `notes` `notes` TINYTEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
+''')
+        c.execute('''
+ALTER TABLE `ClientIntoleranceMedicament`
+    CHANGE COLUMN `nameMedicament` `nameMedicament` VARCHAR(128) NOT NULL COMMENT 'Название медикамента' AFTER `client_id`,
+    CHANGE COLUMN `notes` `notes` TINYTEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
+''')
+
+
+
+
+
