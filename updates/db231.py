f7c3d3b70271b8b5b61420f32a1c3b74dea55b50
Михаил Мальков <m.malkov@bars-open.ru>
2016-05-06 14:24:40 +0300

    Update 231 For TMIS-1066 Fix

--- updates/db231.py
+++ updates/db231.py
@@ -10,36 +10,47 @@ __doc__ = '''\
 
 def upgrade(conn):
     with conn as c:
-        c.execute('''
+        c.execute(u'''
 ALTER TABLE `ClientAllergy`
     ALTER `nameSubstance` DROP DEFAULT;
 ALTER TABLE `ClientAllergy`
     CHANGE COLUMN `nameSubstance` `nameSubstance` TEXT NOT NULL COMMENT 'Наименование вещества' AFTER `client_id`,
     CHANGE COLUMN `notes` `notes` TEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
 ''')
-        c.execute('''
+        c.execute(u'''
 ALTER TABLE `ClientIntoleranceMedicament`
     ALTER `nameMedicament` DROP DEFAULT;
 ALTER TABLE `ClientIntoleranceMedicament`
     CHANGE COLUMN `nameMedicament` `nameMedicament` TEXT NOT NULL COMMENT 'Название медикамента' AFTER `client_id`,
     CHANGE COLUMN `notes` `notes` TEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
 ''')
+        c.execute(u'''
+ALTER TABLE `ClientContact`
+    ALTER `contact` DROP DEFAULT,
+    ALTER `notes` DROP DEFAULT;
+ALTER TABLE `ClientContact`
+    CHANGE COLUMN `contact` `contact` TEXT NOT NULL COMMENT 'Контакт' AFTER `contactType_id`,
+    CHANGE COLUMN `notes` `notes` TEXT NOT NULL COMMENT 'Контакт' AFTER `contact`;
+''')
 
 
 def downgrade(conn):
     with conn as c:
-        c.execute('''
+        c.execute(u'''
 ALTER TABLE `ClientAllergy`
     CHANGE COLUMN `nameSubstance` `nameSubstance` VARCHAR(128) NOT NULL COMMENT 'Наименование вещества' AFTER `client_id`,
     CHANGE COLUMN `notes` `notes` TINYTEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
 ''')
-        c.execute('''
+        c.execute(u'''
 ALTER TABLE `ClientIntoleranceMedicament`
     CHANGE COLUMN `nameMedicament` `nameMedicament` VARCHAR(128) NOT NULL COMMENT 'Название медикамента' AFTER `client_id`,
     CHANGE COLUMN `notes` `notes` TINYTEXT NOT NULL COMMENT 'Примечание' AFTER `createDate`
 ''')
-
-
-
-
-
+        c.execute(u'''
+ALTER TABLE `ClientContact`
+    ALTER `contact` DROP DEFAULT,
+    ALTER `notes` DROP DEFAULT;
+ALTER TABLE `ClientContact`
+    CHANGE COLUMN `contact` `contact` VARCHAR(32) NOT NULL COMMENT 'Контакт' AFTER `contactType_id`,
+    CHANGE COLUMN `notes` `notes` VARCHAR(64) NOT NULL COMMENT 'Контакт' AFTER `contact`;
+''')
